5dbce313d0ca44063126d2b487eefe4cfd04a4a2
ayrton <ayrton.arc@gmail.com>
2021-02-18 21:44:53 -0800

    manejando informacion con interfaces

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RespuestaTopHeadlines } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class NoticiasService {
   constructor( private http: HttpClient ) { }
 
   getTopHeadlines() {
-    return this.http.get(`http://newsapi.org/v2/top-headlines?country=mx&apiKey=cdc72a0e91f14267835c4d2ac5490222`);
+    return this.http.get<RespuestaTopHeadlines>(`http://newsapi.org/v2/top-headlines?country=mx&apiKey=cdc72a0e91f14267835c4d2ac5490222`);
   }
 }
